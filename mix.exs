9384889345b572044c642fe27b3d894d3e9fac92
Attila Gulyas <toraritte@gmail.com>
2018-10-31 09:17:28 -0700

    Add back evenstore to Mixfile
    See notes in  `mix.exs` but the gist is that  the adapter implementation
    is tightly  coupled with  `commanded/eventstore`, using  structs defined
    there, thus it needs to be included.

--- mix.exs
+++ mix.exs
@@ -20,10 +20,14 @@ defmodule Commanded.EventStore.Adapters.EventStore.Mixfile do
   end
 
   """
+  NOTE 2018-10-30_2231
+
   Removing  `eventstore`  because   it  is  listed  in
-  `deps`   therefore   it   is   added   automatically
-  to  `:applications`.   Although  this  app   is  not
-  supervised.
+  `deps`  therefore  it   is  added  automatically  to
+  `:applications`. This  app also  does not  start any
+  processes,  it  just  maps  behaviour  callbacks  to
+  EventStore functions. EventStore  should be added to
+  a projects `:extra_applications` anyway.
 
   https://hexdocs.pm/mix/Mix.Tasks.Compile.App.html
   """
@@ -47,22 +51,68 @@ defmodule Commanded.EventStore.Adapters.EventStore.Mixfile do
   """
   NOTE 2018-10-30_2231
 
-  + Remove Commanded as a dependency, see issue #1.
+    + Remove Commanded as a dependency, see issue #1.
 
-    => Maybe adding it as a test dependency?
+      => Maybe adding it as a test dependency?
 
-  + Also removing EventStore.
+    + Also removing EventStore.
 
-    I  am  biased  toward  more  explicit  configuration
-    and the  `EventStore` application will  be available
-    in   a   project,    therefore   the   mappings   in
-    `./lib/event_store_adapter.ex` will work.
+      I  am  biased  toward  more  explicit  configuration
+      and the  `EventStore` application will  be available
+      in   a   project,    therefore   the   mappings   in
+      `./lib/event_store_adapter.ex` will work.
 
-    => Test dependency?
+      => Test dependency?
 
-  + The `Mox` idea seems to be  a nice approach as a 3rd
-    option to the ones mentioned in issue #1. Explore it
-    further.
+    + The `Mox` idea seems to be  a nice approach as a 3rd
+      option to the ones mentioned in issue #1. Explore it
+      further.
+
+  NOTE 2018-10-31_0833
+
+    Adding   back  `eventstore`   as  this   adapter  is
+    basically only  a wrapper  around EventStore  and it
+    uses EventStore's  structs directly,  therefore they
+    need to be present.
+
+    => In this case, why not make it an application that
+       automatically adds EventStore  to the supervision
+       tree to any project that includes this adapter in
+       its deps?
+      \
+       I  think  it would  be  a  bad idea  wrapping  an
+       application  into  another  application  just  to
+       maintain conformity.
+
+    Here's a radical idea:
+    ----------------------
+    This   `commanded/eventstore`    vs   Greg   Young's
+    EventStore   situation  is   exactly  the   same  as
+    with  `Phoenix.PubSub`:  there   are  two  adapters,
+    an   internal   (`PG2`)    and   an   external   one
+    (`Redis`).   The   `phoenix_pubsub`  repo   contains
+    the  adapter  specification  (`Phoenix.PubSub`)  and
+    internal  adapter implementation  (`PG2`), which  is
+    basically  the  PubSub  implementation  adhering  to
+    the specs  in the same  repo. `phoenix_pubsub_redis`
+    however    is   a    completely   different    repo,
+    including   `phoenix_pubsub`   as  its   dependency,
+    and  it  indeed  is   a  translation  layer  between
+    the   Redis  service   and  Phoenix.   Meanwhile  it
+    draws   on  several   `phoenix_pubsub`  modules   to
+    avoid  implementing  common functionality  (such  as
+    `Phoenix.PubSub.LocalSupervisor`).
+
+    Long  story  short,  Commanded   also  has  its  own
+    EventStore  in  a separate  repo,  but  it would  be
+    natural if  it would  "just" conform to  the adapter
+    spec, and the Extreme adapter would be the bridge to
+    GY's EventStore. This is of course more nuanced than
+    that, but will figure it out.
+
+    Until  then,  all  remains the  same:  include  this
+    adapter  in  a  project   and  add  `eventstore`  to
+    `:extra_applications`.
   """
   defp deps do
     [
